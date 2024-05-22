317d5f5817825eec2f6fd197ea7f16f4e5ec66d5
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2024-05-22 17:54:38 +0200

    a

--- swiper-bangers.js
+++ swiper-bangers.js
@@ -2,29 +2,28 @@ const swiper4 = new Swiper('.swiper-bangers', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: "auto",
-    centeredSlides: true,
+    slidesPerView: 3,
     spaceBetween: 200,
     allowTouchMove: true,
-    pauseOnMouseEnter: true,
     autoplay: {
       delay: 4500,
       reverseDirection: true,
+      pauseOnMouseEnter: true,
     },
     breakpoints: {
       // when window width is >= 320px
       400: {
-        slidesPerView: "auto",
+        slidesPerView: 3,
         spaceBetween: 20
       },
       // when window width is >= 480px
       550: {
-        slidesPerView: "auto",
+        slidesPerView: 3,
         spaceBetween: 20
       },
       // when window width is >= 640px
       830: {
-        slidesPerView: "auto",
+        slidesPerView: 3,
         spaceBetween: 20
       }
     },
