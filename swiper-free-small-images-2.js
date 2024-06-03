82c39b112b0dae08ab9412c11b2a699fc3cb60aa
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2024-06-03 20:29:23 +0200

    a

---
+++ swiper-free-small-images-2.js
@@ -0,0 +1,31 @@
+const swiper10 = new Swiper('.swiper-free-small-images-2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3500,
+      reverseDirection: true,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      400: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      650: {
+        slidesPerView: 7,
+        spaceBetween: 20
+      },
+      // when window width is >= 640px
+      830: {
+        slidesPerView: 9,
+        spaceBetween: 20
+      }
+    },
+
+  
+  });
