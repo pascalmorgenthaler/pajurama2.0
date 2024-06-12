6a7a6c3fdf0b888d9dfa53fa13b0afe9afac0767
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2024-06-12 15:58:26 +0200

    q
    q

--- anfrage.js
+++ anfrage.js
@@ -12,6 +12,11 @@
     document.getElementById("personalbranding-auswahl").classList.add('hidden');
     document.getElementById("unternehmen-auswahl").classList.add('hidden');
     document.getElementById("tourismus-auswahl").classList.add('hidden');
+    document.getElementById("brand-auswahl").classList.add('hidden');
+    document.getElementById("hotel-auswahl").classList.add('hidden');
+    document.getElementById("fashion-auswahl").classList.add('hidden');
+    document.getElementById("cafe-auswahl").classList.add('hidden');
+    document.getElementById("etwas-anderes-auswahl").classList.add('hidden');
   };
 
 document.getElementById('fotografie-checkbox').addEventListener('click', function() {
@@ -25,7 +30,21 @@ document.getElementById('fotografie-checkbox').addEventListener('click', functio
     // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
     document.getElementById("tourismus-auswahl").classList.add('hidden');
     document.getElementById('tourismus-checkbox').checked = false;
-
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("brand-auswahl").classList.add('hidden');
+    document.getElementById('brand-checkbox').checked = false;
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("hotel-auswahl").classList.add('hidden');
+    document.getElementById('hotel-checkbox').checked = false;
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("fashion-auswahl").classList.add('hidden');
+    document.getElementById('fashion-checkbox').checked = false;
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("cafe-auswahl").classList.add('hidden');
+    document.getElementById('cafe-checkbox').checked = false;
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("etwas-anderes-auswahl").classList.add('hidden');
+    document.getElementById('etwas-anderes-checkbox').checked = false;
 });
 
 document.getElementById('personalbranding-checkbox').addEventListener('click', function() {
@@ -40,3 +59,23 @@ document.getElementById('tourismus-checkbox').addEventListener('click', function
     document.getElementById("tourismus-auswahl").classList.toggle('hidden');
 });
 
+document.getElementById('brand-checkbox').addEventListener('click', function() {
+    document.getElementById("brand-auswahl").classList.toggle('hidden');
+});
+
+document.getElementById('hotel-checkbox').addEventListener('click', function() {
+    document.getElementById("hotel-auswahl").classList.toggle('hidden');
+});
+
+document.getElementById('fashion-checkbox').addEventListener('click', function() {
+    document.getElementById("fashion-auswahl").classList.toggle('hidden');
+});
+
+document.getElementById('cafe-checkbox').addEventListener('click', function() {
+    document.getElementById("cafe-auswahl").classList.toggle('hidden');
+});
+
+
+document.getElementById('etwas-anderes-checkbox').addEventListener('click', function() {
+    document.getElementById("etwas-anderes-auswahl").classList.toggle('hidden');
+});
