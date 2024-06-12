2381fd1c74c6e4df6a4da8dcf673f902f2a7234a
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2024-06-12 15:42:08 +0200

    a

--- anfrage.js
+++ anfrage.js
@@ -4,12 +4,39 @@
 //     document.getElementById("fotografie-check").classList.remove('hidden');
 // };
 
+// window.addEventListener("load", (event) => {
+//     document.getElementById("personalbranding-auswahl").classList.add('hidden');
+//   });
 
+  window.onload = (event) => {
+    document.getElementById("personalbranding-auswahl").classList.add('hidden');
+    document.getElementById("unternehmen-auswahl").classList.add('hidden');
+    document.getElementById("tourismus-auswahl").classList.add('hidden');
+  };
 
 document.getElementById('fotografie-checkbox').addEventListener('click', function() {
     document.getElementById("fotografie-auswahl").classList.toggle('hidden');
+    // CLOSE personalbranding when form is open and uncheck personalbranding-checkbox
+    document.getElementById("personalbranding-auswahl").classList.add('hidden');
+    document.getElementById('personalbranding-checkbox').checked = false;
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("unternehmen-auswahl").classList.add('hidden');
+    document.getElementById('unternehmen-checkbox').checked = false;
+    // CLOSE personalbranding when form is open and uncheck unternehmen-checkbox
+    document.getElementById("tourismus-auswahl").classList.add('hidden');
+    document.getElementById('tourismus-checkbox').checked = false;
+
 });
 
+document.getElementById('personalbranding-checkbox').addEventListener('click', function() {
+    document.getElementById("personalbranding-auswahl").classList.toggle('hidden');
+});
 
+document.getElementById('unternehmen-checkbox').addEventListener('click', function() {
+    document.getElementById("unternehmen-auswahl").classList.toggle('hidden');
+});
 
-console.log('hy');
+document.getElementById('tourismus-checkbox').addEventListener('click', function() {
+    document.getElementById("tourismus-auswahl").classList.toggle('hidden');
+});
+
