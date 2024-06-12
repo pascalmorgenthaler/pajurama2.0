8ba4a7202435de4ec9c2e6a1533f627611962e0f
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2024-06-12 12:09:07 +0200

    a

---
+++ anfrage.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// if(document.getElementById("fotografie").checked = true) {
+//     document.getElementById("fotografie-check").classList.remove('hidden');
+// };
+
+
+
+document.getElementById('fotografie-checkbox').addEventListener('click', function() {
+    document.getElementById("fotografie-auswahl").classList.toggle('hidden');
+});
+
+
+
+console.log('hy');
